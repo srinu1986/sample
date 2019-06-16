2fae32c2e8b257beee3066e1f01213c3a2696159
srinu1986 <36822340+srinu1986@users.noreply.github.com>
2019-06-17 02:43:38 +0530

    Add files via upload

---
+++ src/main/java/com/srinu/small/App.java
@@ -0,0 +1,13 @@
+package com.srinu.small;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
